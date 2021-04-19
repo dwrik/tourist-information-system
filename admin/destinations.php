ab706c12b3eac8f07158a292767c5f20a2a61234
Wrik Das <wrikdas22@gmail.com>
2021-04-19 12:54:31 +0530

    Added more styles

--- admin/destinations.php
+++ admin/destinations.php
@@ -1,83 +1,84 @@
 <?php
-    session_start();
+session_start();
 
-    // redirect back to login if not authorized
-    if (!isset($_SESSION['auth']) || $_SESSION['auth'] != true) {
-        header('location: login.php');
-        exit;
+// redirect back to login if not authorized
+if (!isset($_SESSION['auth']) || $_SESSION['auth'] != true) {
+    header('location: login.php');
+    exit;
+}
+
+include('../database/connection.php');
+
+$upload_err = '';
+
+function validateImage($image)
+{
+    // get file info
+    $file_name = $_FILES[$image]['name'];
+    $file_size = $_FILES[$image]['size'];
+    $tmp_name = $_FILES[$image]['tmp_name'];
+
+    $extensions = ['png', 'jpg', 'jpeg'];
+    $file_name_parts = explode('.', $file_name);
+    $file_ext = strtolower($file_name_parts[count($file_name_parts) - 1]);
+
+    // limit extensions
+    if (!in_array($file_ext, $extensions)) {
+        return false;
     }
 
-    include('../database/connection.php');
-
-    $upload_err = '';
-
-    function validateImage($image) {
-        // get file info
-        $file_name = $_FILES[$image]['name'];
-        $file_size = $_FILES[$image]['size'];
-        $tmp_name = $_FILES[$image]['tmp_name'];
-
-        $extensions = ['png', 'jpg', 'jpeg'];
-        $file_name_parts = explode('.', $file_name);
-        $file_ext = strtolower($file_name_parts[count($file_name_parts) - 1]);
-
-        // limit extensions
-        if (!in_array($file_ext, $extensions)) {
-            return false;
-        }
-
-        // limit file size to 10 MB
-        if ($file_size > 10000000) {
-            return false;
-        }
-
-        // generate random filename with timestamp
-        $path = '../images/destinations/';
-        $filename = $path . uniqid(rand(1, 999)) . '.' . $file_ext;
-
-        return ['tmp_name' => $tmp_name, 'filename' => $filename];
+    // limit file size to 10 MB
+    if ($file_size > 10000000) {
+        return false;
     }
 
-    if (isset($_POST['name'])) {
-        // validate images uploaded
-        $valid_cover = validateImage('cover', '../images/destinations/');
+    // generate random filename with timestamp
+    $path = '../images/destinations/';
+    $filename = $path . uniqid(rand(1, 999)) . '.' . $file_ext;
 
-        if ($valid_cover) {
-            // add record
-            if ($_GET['operation'] == 'add') {
-                $query = 'INSERT INTO DESTINATIONS (CATEGORY_ID, NAME, DESCRIPTION, COVER) VALUES ('
-                    . '"' . $_GET['category']        . '", '
-                    . '"' . $_POST['name']           . '", '
-                    . '"' . $_POST['description']    . '", '
-                    . '"' . $valid_cover['filename'] . '")';
-            }
+    return ['tmp_name' => $tmp_name, 'filename' => $filename];
+}
 
-            // update record
-            if ($_GET['operation'] == 'update') {
-                // delete existing image from image directory
-                $query = 'SELECT * FROM DESTINATIONS WHERE ID=' . $_GET['id'];
-                $row = mysqli_fetch_object(mysqli_query($conn, $query));
-                unlink($row->cover);
+if (isset($_POST['name'])) {
+    // validate images uploaded
+    $valid_cover = validateImage('cover', '../images/destinations/');
 
-                $query = 'UPDATE DESTINATIONS SET '
-                    . 'NAME="' . $_POST['name']               . '", '
-                    . 'DESCRIPTION="' . $_POST['description'] . '", '
-                    . 'COVER="' . $valid_cover['filename']    . '" '
-                    . 'WHERE ID=' . $_GET['id'];
-            }
-
-            // common operations b/w add and update
-
-            move_uploaded_file($valid_cover['tmp_name'], $valid_cover['filename']);
-
-            $result = mysqli_query($conn, $query);
-            if ($result == false) {
-                $upload_err = 'Oops something went wrong!';
-            }
-        } else {
-            $upload_err = 'Invalid file extension. Please upload a valid image!';
+    if ($valid_cover) {
+        // add record
+        if ($_GET['operation'] == 'add') {
+            $query = 'INSERT INTO DESTINATIONS (CATEGORY_ID, NAME, DESCRIPTION, COVER) VALUES ('
+                . '"' . $_GET['category']        . '", '
+                . '"' . $_POST['name']           . '", '
+                . '"' . $_POST['description']    . '", '
+                . '"' . $valid_cover['filename'] . '")';
         }
+
+        // update record
+        if ($_GET['operation'] == 'update') {
+            // delete existing image from image directory
+            $query = 'SELECT * FROM DESTINATIONS WHERE ID=' . $_GET['id'];
+            $row = mysqli_fetch_object(mysqli_query($conn, $query));
+            unlink($row->cover);
+
+            $query = 'UPDATE DESTINATIONS SET '
+                . 'NAME="' . $_POST['name']               . '", '
+                . 'DESCRIPTION="' . $_POST['description'] . '", '
+                . 'COVER="' . $valid_cover['filename']    . '" '
+                . 'WHERE ID=' . $_GET['id'];
+        }
+
+        // common operations b/w add and update
+
+        move_uploaded_file($valid_cover['tmp_name'], $valid_cover['filename']);
+
+        $result = mysqli_query($conn, $query);
+        if ($result == false) {
+            $upload_err = 'Oops something went wrong!';
+        }
+    } else {
+        $upload_err = 'Invalid file extension. Please upload a valid image!';
     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -85,57 +86,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/login.css">
     <title>Places</title>
 </head>
 <body>
-    <h1>Admin Portal</h1>
-    <table>
-        <tr>
-            <td><strong>Name</strong>: <?php echo htmlspecialchars($_SESSION['user']->name); ?></td>
-            <td><strong>Email</strong>: <?php echo htmlspecialchars($_SESSION['user']->email); ?></td>
-            <td><a href="logout.php">Logout</a></td>
-        </tr>
-    </table>
-    <hr>
-    <h2><?php echo $_GET['operation'] ?> place</h2>
-    <?php
-        // set initial field values
-        $name = '';
-        $description = '';
+    <section id="services" class="features-area">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 col-md-10">
+                    <div class="section-title text-center pb-10">
+                        <h1>Admin Portal</h1>
 
-        if ($_GET['operation'] == 'update') {
-            $query = 'SELECT * FROM DESTINATIONS WHERE ID=' . $_GET['id'];
-            $result = mysqli_query($conn, $query);
-            $row = mysqli_fetch_object($result);
+                        <strong>Name</strong>: <?php echo htmlspecialchars($_SESSION['user']->name); ?>
+                        <strong>Email</strong>: <?php echo htmlspecialchars($_SESSION['user']->email); ?>
+                        <a href="logout.php">Logout</a></td>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <?php
+            // set initial field values
+            $name = '';
+            $description = '';
 
-            $name = $row->name;
-            $description = $row->description;
-        }
-    ?>
-    <form action="" method="POST" enctype="multipart/form-data">
-        <table>
-            <tr>
-                <td>Name</td>
-                <td><input type="text" name="name" value="<?php echo $name; ?>" maxlength=80 required></td>
-            </tr>
-            <tr>
-                <td>Description</td>
-                <td><input type="text" name="description" value="<?php echo $description; ?>" maxlength=800 required></td>
-            </tr>
-            <tr>
-                <td>Cover</td>
-                <td><input type="file" name="cover" accept="image/png, image/jpeg" required></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td style="color: red;"><?php echo $upload_err ?></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" value="Save"></td>
-            </tr>
-        </table>
-    </form>
-    <a href=<?php echo "home.php?category=" . $_GET['category'] ?>>Back</a>
+            if ($_GET['operation'] == 'update') {
+                $query = 'SELECT * FROM DESTINATIONS WHERE ID=' . $_GET['id'];
+                $result = mysqli_query($conn, $query);
+                $row = mysqli_fetch_object($result);
+
+                $name = $row->name;
+                $description = $row->description;
+            }
+            ?>
+            <div class="wrapper">
+                <div id="formContent">
+                    <h2><?php echo $_GET['operation'] ?> place</h2>
+                    <form action="" method="POST" enctype="multipart/form-data">
+                        <input type="text" name="name" placeholder="Name" value="<?php echo $name; ?>" maxlength=80 required>
+                        <input type="text" name="description" placeholder="Description" value="<?php echo $description; ?>" maxlength=800 required>
+                        <input type="file" name="cover" placeholder="Cover Image" accept="image/png, image/jpeg" required>
+                        <p style="color: red;"><?php echo $upload_err ?></p>
+                        <td><input type="submit" value="Save"></td>
+                    </form>
+                    <a href=<?php echo "home.php?category=" . $_GET['category'] ?>>Back</a>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
--- admin/home.php
+++ admin/home.php
@@ -1,62 +1,64 @@
 <?php
-    session_start();
+session_start();
 
-    // redirect back to login if not authorized
-    if (!isset($_SESSION['auth']) || $_SESSION['auth'] != true) {
-        header('location: login.php');
-        exit;
-    }
+// redirect back to login if not authorized
+if (!isset($_SESSION['auth']) || $_SESSION['auth'] != true) {
+    header('location: login.php');
+    exit;
+}
 
-    include('../database/connection.php');
+include('../database/connection.php');
 
-    // helper
-    function deleteImage($conn, $table, $id) {
-        $query = 'SELECT * FROM ' . $table . ' WHERE ID=' . $id;
+// helper
+function deleteImage($conn, $table, $id)
+{
+    $query = 'SELECT * FROM ' . $table . ' WHERE ID=' . $id;
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_object($result);
+    unlink($row->cover);
+}
+
+// helper
+function deleteRecord($conn, $table, $id)
+{
+    $query = 'DELETE FROM ' . $table . ' WHERE ID=' . $id;
+    mysqli_query($conn, $query);
+}
+
+if (isset($_GET['approve'])) {
+    if ($_GET['approve'] == 'true') {
+        $query = 'SELECT * FROM PENDING WHERE ID=' . $_GET['id'];
         $result = mysqli_query($conn, $query);
         $row = mysqli_fetch_object($result);
-        unlink($row->cover);
-    }
 
-    // helper
-    function deleteRecord($conn, $table, $id) {
-        $query = 'DELETE FROM ' . $table . ' WHERE ID=' . $id;
+        $query = 'INSERT INTO ADMINS (NAME, EMAIL, PASSWORD) VALUES ('
+            . '"' . $row->name     . '", '
+            . '"' . $row->email    . '", '
+            . '"' . $row->password . '")';
         mysqli_query($conn, $query);
     }
+    deleteRecord($conn, 'PENDING', $_GET['id']);
+    header('location: home.php');
+}
 
-    if (isset($_GET['approve'])) {
-        if ($_GET['approve'] == 'true') {
-            $query = 'SELECT * FROM PENDING WHERE ID=' . $_GET['id'];
-            $result = mysqli_query($conn, $query);
-            $row = mysqli_fetch_object($result);
-
-            $query = 'INSERT INTO ADMINS (NAME, EMAIL, PASSWORD) VALUES ('
-                . '"' . $row->name     . '", '
-                . '"' . $row->email    . '", '
-                . '"' . $row->password . '")';
-            mysqli_query($conn, $query);
+// delete record if id is set or delete entire category
+if (isset($_GET['operation']) && $_GET['operation'] == 'delete') {
+    if (isset($_GET['id'])) {
+        deleteImage($conn, 'DESTINATIONS', $_GET['id']);
+        deleteRecord($conn, 'DESTINATIONS', $_GET['id']);
+        header('location: home.php?category=' . $_GET['category']);
+    } else {
+        $query = 'SELECT * FROM DESTINATIONS WHERE CATEGORY_ID=' . $_GET['category'];
+        $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_object($result)) {
+            deleteImage($conn, 'DESTINATIONS', $row->id);
+            deleteRecord($conn, 'DESTINATIONS', $row->id);
         }
-        deleteRecord($conn, 'PENDING', $_GET['id']);
+        deleteImage($conn, 'CATEGORIES', $_GET['category']);
+        deleteRecord($conn, 'CATEGORIES', $_GET['category']);
         header('location: home.php');
     }
-
-    // delete record if id is set or delete entire category
-    if (isset($_GET['operation']) && $_GET['operation'] == 'delete') {
-        if (isset($_GET['id'])) {
-            deleteImage($conn, 'DESTINATIONS', $_GET['id']);
-            deleteRecord($conn, 'DESTINATIONS', $_GET['id']);
-            header('location: home.php?category=' . $_GET['category']);
-        } else {
-            $query = 'SELECT * FROM DESTINATIONS WHERE CATEGORY_ID=' . $_GET['category'];
-            $result = mysqli_query($conn, $query);
-            while ($row = mysqli_fetch_object($result)) {
-                deleteImage($conn, 'DESTINATIONS', $row->id);
-                deleteRecord($conn, 'DESTINATIONS', $row->id);
-            }
-            deleteImage($conn, 'CATEGORIES', $_GET['category']);
-            deleteRecord($conn, 'CATEGORIES', $_GET['category']);
-            header('location: home.php');
-        }
-    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -74,7 +76,7 @@
             <div class="row justify-content-center">
                 <div class="col-lg-6 col-md-10">
                     <div class="section-title text-center pb-10">
-                        <h3 class="title">Admin Portal</h3>
+                        <h1>Admin Portal</h1>
                         <strong>Name</strong>: <?php echo htmlspecialchars($_SESSION['user']->name); ?>
                         <strong>Email</strong>: <?php echo htmlspecialchars($_SESSION['user']->email); ?>
                         <a href="logout.php">Logout</a>
@@ -83,105 +85,106 @@
             </div> <!-- row -->
             <hr>
             <?php
-                // show selected category
-                if (isset($_GET['category'])) {
-                    $category_id = $_GET['category'];
+            // show selected category
+            if (isset($_GET['category'])) {
+                $category_id = $_GET['category'];
 
-                    // get category name
-                    $query = 'SELECT * FROM CATEGORIES WHERE ID=' . $category_id;
-                    $result = mysqli_query($conn, $query);
-                    $row = mysqli_fetch_object($result);
-                    $category_name = $row->name;
+                // get category name
+                $query = 'SELECT * FROM CATEGORIES WHERE ID=' . $category_id;
+                $result = mysqli_query($conn, $query);
+                $row = mysqli_fetch_object($result);
+                $category_name = $row->name;
 
-                    // get destinations in category
-                    $query = 'SELECT * FROM DESTINATIONS WHERE CATEGORY_ID=' . $category_id;
-                    $result = mysqli_query($conn, $query);
+                // get destinations in category
+                $query = 'SELECT * FROM DESTINATIONS WHERE CATEGORY_ID=' . $category_id;
+                $result = mysqli_query($conn, $query);
             ?>
-                    <h2><?php echo $category_name ?></h2>
-                    <a style="margin:10px 0px" href="destinations.php?category=<?php echo $category_id ?>&operation=add">
-                        Add Place
-                    </a>
-                    <table>
-                        <?php while ($row = mysqli_fetch_object($result)) { ?>
+                <h2><?php echo $category_name ?></h2>
+                <a style="margin:10px 0px" href="destinations.php?category=<?php echo $category_id ?>&operation=add">
+                    Add Place
+                </a>
+                <table>
+                    <?php while ($row = mysqli_fetch_object($result)) { ?>
+                        <tr>
+                            <td>
+                                <img width=50 height=40 src="<?php echo $row->cover ?>" alt="Cover">
+                            </td>
+                            <td><?php echo $row->name ?></td>
+                            <td>
+                                <a href=<?php echo 'destinations.php?'
+                                            . 'category=' . $category_id
+                                            . '&operation=update'
+                                            . '&id=' . $row->id; ?>>Update</a>
+                            </td>
+                            <td>|</td>
+                            <td>
+                                <a href=<?php echo '?'
+                                            . 'category=' . $category_id
+                                            . '&operation=delete'
+                                            . '&id=' . $row->id; ?>>Delete</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </table>
+                <br>
+                <a href="home.php">Back</a>
+                <?php
+            } else {
+                $query = 'SELECT * FROM PENDING';
+                $result = mysqli_query($conn, $query);
+                if (mysqli_num_rows($result) > 0) {
+                    echo '<h2>Notifications</h2>';
+                    while ($row = mysqli_fetch_object($result)) {
+                ?>
+                        <table>
                             <tr>
-                                <td>
-                                    <img width=50 height=40 src="<?php echo $row->cover ?>" alt="Cover">
-                                </td>
-                                <td><?php echo $row->name ?></td>
-                                <td>
-                                    <a href=<?php echo 'destinations.php?'
-                                        . 'category=' . $category_id
-                                        . '&operation=update'
-                                        . '&id=' . $row->id; ?>>Update</a>
-                                </td>
-                                <td>|</td>
-                                <td>
-                                    <a href=<?php echo '?'
-                                        . 'category=' . $category_id
-                                        . '&operation=delete'
-                                        . '&id=' . $row->id; ?>>Delete</a>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                    </table>
-                    <br>
-                    <a href="home.php">Back</a>
-            <?php
-                // show all categories
-                } else {
-                    $query = 'SELECT * FROM PENDING';
-                    $result = mysqli_query($conn, $query);
-                    if (mysqli_num_rows($result) > 0) {
-                        echo '<h2>Notifications</h2>';
-                        while ($row = mysqli_fetch_object($result)) {
-            ?>
-                            <table>
-                                <tr>
-                                    <td><?php echo $row->name
+                                <td><?php echo $row->name
                                         . ' (' . $row->email . ') ' ?> has requested admin acess.</td>
-                                    <td><a href="?id=<?php echo $row->id ?>&approve=true">Approve</a></td>
-                                    <td>|</td>
-                                    <td><a href="?id=<?php echo $row->id ?>&approve=false">Reject</a></td>
-                                </tr>
-                            </table>
-            <?php
-                        }
-                    }   
-                    $query = 'SELECT * FROM CATEGORIES';
-                    $result = mysqli_query($conn, $query);
-            ?>
-                    <h2>Categories</h2>
-                    <a style="margin:10px 0px" href="categories.php?operation=add">
-                        Add Category
-                    </a>
-                    <table>
-                <?php while ($row = mysqli_fetch_object($result)) { ?>
-                            <tr>
-                                <td>
-                                    <img width=50 height=40 src="<?php echo $row->cover ?>" alt="Cover">
-                                </td>
-                                <td>
-                                    <a href="?category=<?php echo $row->id ?>"><?php echo $row->name ?></a>
-                                </td>
-                                <td>
-                                    <a href=<?php echo 'categories.php?'
-                                        . 'id=' . $row->id
-                                        . '&operation=update'?>>Update</a>
-                                </td>
+                                <td><a href="?id=<?php echo $row->id ?>&approve=true">Approve</a></td>
                                 <td>|</td>
-                                <td>
-                                    <a href=<?php echo '?category='
-                                        . $row->id . '&operation=delete' ?>>Delete</a>
-                                </td>
+                                <td><a href="?id=<?php echo $row->id ?>&approve=false">Reject</a></td>
                             </tr>
+                        </table>
                 <?php
                     }
+                }
+                // show all categories
+                $query = 'SELECT * FROM CATEGORIES';
+                $result = mysqli_query($conn, $query);
                 ?>
+                <h2>Categories</h2>
+                <a style="margin:10px 0px" href="categories.php?operation=add">
+                    Add Category
+                </a>
+                <table>
+                    <?php while ($row = mysqli_fetch_object($result)) { ?>
+                        <tr>
+                            <td>
+                                <img width=50 height=40 src="<?php echo $row->cover ?>" alt="Cover">
+                            </td>
+                            <td>
+                                <a href="?category=<?php echo $row->id ?>"><?php echo $row->name ?></a>
+                            </td>
+                            <td>
+                                <a href=<?php echo 'categories.php?'
+                                            . 'id=' . $row->id
+                                            . '&operation=update' ?>>Update</a>
+                            </td>
+                            <td>|</td>
+                            <td>
+                                <a href=<?php echo '?category='
+                                            . $row->id . '&operation=delete' ?>>Delete</a>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
                 </table>
             <?php
-                }
+            }
             ?>
         </div> <!-- container -->
     </section>
 </body>
+
 </html>
